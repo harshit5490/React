44435598292bd3a5585cb4242b0ef5c3de2754ca
harshit5490 <harshithg062002@gmail.com>
2024-05-17 16:55:59 +0530

    firstCommit

---
+++ MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+import conf from './conf/conf'
+import './App.css'
+
+function App() {
+  console.log(conf.appwriteUrl);
+
+  return (
+    <>
+      <h1>A Blog App with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
