d4dce4efd27ac1f57bd187d7595ba0a4aa700ca6
harshit5490 <harshithg062002@gmail.com>
2024-06-02 03:04:00 +0530

    pagesCreated

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -6,6 +6,11 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
 import RTE from "./RTE";
+import Login from "./Login";
+import Signup from "./Signup";
+import Select from "./Select";
+import PostCard from "./PostCard";  
+import PostForm from "./post-form/PostForm";
 
 
-export {Footer,Header,Logo,Container,LogoutBtn,Button,Input,RTE}
+export {Footer,Header,Logo,Container,LogoutBtn,Button,Input,RTE,Login,Select,Signup,PostCard,PostForm}
