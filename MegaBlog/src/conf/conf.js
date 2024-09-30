a7f33ba8d1599b482033ff4135deaeab0a414b27
harshit5490 <harshithg062002@gmail.com>
2024-09-30 17:45:49 +0530

    delete file

--- MegaBlog/src/conf/conf.js
+++
@@ -1,9 +0,0 @@
-const conf = {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwritDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwritCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwritBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    tinymceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
-}
-export default conf;
